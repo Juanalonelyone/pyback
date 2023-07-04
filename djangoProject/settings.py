fe1e9afc22187ca1f55a44dca87f822d6d4f34fe
徐锦华 <1019421426@qq.com>
2023-07-04 14:22:00 +0800

    完成摄像头在前端web的实时获取与展示视频流

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'worker',
     'event',
     'vol',
-    'video_catch'
+    'video_catch',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://128.0.0.1',
+    'http://127.0.0.1:5173'
+]
--- video_catch/views_video_catch.py
+++ video_catch/views_video_catch.py
@@ -7,27 +7,23 @@ import cv2
 import time
 
 
-
 def video_catch(request):
     # 创建 VideoCapture 对象
     cap = cv2.VideoCapture(0)
+    print(1111111)
 
     def generate_frames():
+        print(2222)
         while True:
             # 读取摄像头帧
             ret, frame = cap.read()
-
-            # 将帧转换为 JPEG 格式
-            ret, jpeg = cv2.imencode('.jpg', frame)
-
-            # 将 JPEG 数据作为流响应返回
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-
-
+            if ret:
+                # 将帧转换为 JPEG 格式
+                ret, frame = cv2.imencode('.jpeg', frame)
+                if ret:
+                    # 将 JPEG 数据作为流响应返回
+                    print(3333)
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
     # 返回 StreamingHttpResponse 响应
     return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
-
-
-
-
