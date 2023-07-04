11c1687e1adf5f248295a66c5c1ab96f3d6fd2bd
徐锦华 <1019421426@qq.com>
2023-07-04 14:57:58 +0800

    完成人脸检测，画框追踪（调库）

--- video_catch/views_video_catch.py
+++ video_catch/views_video_catch.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import StreamingHttpResponse
 import cv2
-import time
+
+# 加载人脸识别模型
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 def video_catch(request):
-    # 创建 VideoCapture 对象
     cap = cv2.VideoCapture(0)
-    print(1111111)
 
     def generate_frames():
-        print(2222)
         while True:
-            # 读取摄像头帧
             ret, frame = cap.read()
             if ret:
+                # 将帧转换为灰度图像
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                # 进行人脸检测
+                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+                # 绘制人脸框
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
                 # 将帧转换为 JPEG 格式
-                ret, frame = cv2.imencode('.jpeg', frame)
+                ret, jpeg = cv2.imencode('.jpeg', frame)
                 if ret:
                     # 将 JPEG 数据作为流响应返回
-                    print(3333)
                     yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
-    # 返回 StreamingHttpResponse 响应
+                           b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+
     return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
