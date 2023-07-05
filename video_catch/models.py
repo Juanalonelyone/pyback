9fc8dbac03ee65efcdde16894a0252107fa7a0a4
wcj <493481936@qq.com>
2023-07-05 10:37:45 +0800

    徐锦华完成人脸识别算法，但识别速度较慢

--- video_catch/models.py
+++ video_catch/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+# Create your models here.
+class OldpersonInfo(models.Model):
+    id = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    id_card = models.CharField(max_length=255, blank=True, null=True)
+    room = models.CharField(max_length=255, blank=True, null=True)
+    img_url = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'oldperson_info'
